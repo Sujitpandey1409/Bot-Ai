f132394211b8cb1ef1558c9d8533f20ff883c915
Sujitpandey1409 <sujitpandey1409@gmail.com>
2024-05-07 16:58:50 +0530

    added functionality with designs for chatting with bot with additional features(scroll to bottom, different starter query etc)

--- botai/src/Components/ChatSection.jsx
+++ botai/src/Components/ChatSection.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./ChatSection.css";
 import ChatLayout from "./ChatLayout";
 import data from "../Assets/data";
@@ -7,6 +7,7 @@ const ChatSection = () => {
   const [currentChat, setCurrentChat] = useState([])
   const [inputData, setInputData] = useState()
   const inputRef = useRef(null)
+  const chatContainerRef = useRef(null);
   const handleAsk = ()=>{
     if(inputData){
       let botReply = 'As an AI Language Model, I don’t have the details'
@@ -19,6 +20,17 @@ const ChatSection = () => {
       inputRef.current.value=''
     }
   }
+  // Function to scroll to the bottom of the chat container
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  };
+
+  // Scroll to bottom on initial render and whenever messages change
+  useEffect(() => {
+    scrollToBottom();
+  }, [currentChat]);
   return (
     <div className="ChatSectionWrapper">
       <div className="titleText">Bot Ai</div>
@@ -33,28 +45,34 @@ const ChatSection = () => {
             Get immediate AI generated response
           </div>
         </div>
-        <div className="query" onClick={()=>{setInputData('Hi, what is the weather');handleAsk()}}>
-          <div className="queryTitle">Hi, what is the weather</div>
+        <div className="query" onClick={()=>{setInputData('Hi, what is my location');handleAsk()}}>
+          <div className="queryTitle">Hi, what is my location</div>
           <div className="queryDefination">
             Get immediate AI generated response
           </div>
         </div>
-        <div className="query" onClick={()=>{setInputData('Hi, what is the weather');handleAsk()}}>
-          <div className="queryTitle">Hi, what is the weather</div>
+        <div className="query" onClick={()=>{setInputData('Hi, what is the temperature');handleAsk()}}>
+          <div className="queryTitle">Hi, what is the temperature</div>
           <div className="queryDefination">
             Get immediate AI generated response
           </div>
         </div>
-        <div className="query" onClick={()=>{setInputData('Hi, what is the weather');handleAsk()}}>
-          <div className="queryTitle" >Hi, what is the weather</div>
+        <div className="query" onClick={()=>{setInputData('Hi, how are you');handleAsk()}}>
+          <div className="queryTitle" >Hi, how are you</div>
           <div className="queryDefination">
             Get immediate AI generated response
           </div>
         </div>
       </div></>:
-      <div className="convoContainer">
-      <ChatLayout query={currentChat[currentChat.length-1].question} />
-      <ChatLayout response={currentChat[currentChat.length-1].response} />
+      <div ref={chatContainerRef} className="convoContainer">
+        {currentChat.map((el)=>{
+        return<>  
+          <ChatLayout query={el.question} />
+          <ChatLayout response={el.response} />
+        </>
+        })}
+      {/* <ChatLayout query={currentChat[currentChat.length-1].question} />
+      <ChatLayout response={currentChat[currentChat.length-1].response} /> */}
       </div>
       }
       <div className="inputAskSave">
